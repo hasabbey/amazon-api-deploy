1b244322ad7e55648b06e884c0f11f3bda220277
hasabbey <abbey2111@gmail.com>
2024-11-12 11:56:07 -0800

    inital commit for amazon backend deployement

---
+++ index.js
@@ -0,0 +1,37 @@
+// const express= require('express');
+const cors= require('cors');
+const dotenv= require('dotenv');
+dotenv.config();
+const stripe=require('stripe')
+(process.env.STRIP_KEY)
+const app =express();
+app.use(cors({orgin:true}))
+app.use(express.json());
+app.get('/', (req, res)=>{
+res.status(200).json({
+    message:"sucessfully recived"
+})
+
+})
+app.post("/payment/create",async(req, res)=>{
+    const total= req.query.total;
+if(total>0){
+    const payment= await stripe.paymentIntents.create({
+        amount:total,
+        currency:"usd"
+              
+    })
+    console.log(payment)
+    res.status(200).json({
+        clientSecret:payment.client_secret,
+    });
+}
+else{
+    res.status(404).json("paymments not working");
+}
+})
+
+app.listen(5000,(err)=>{
+if(err) throw err
+console.log("the servr is running at port http://localhost:5000");
+})
